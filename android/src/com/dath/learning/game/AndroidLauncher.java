21e8045668635f7b47168076103eeae8fd14b591
Jawahardath <dathjava@gmail.com>
2018-02-11 23:32:15 -0500

    Initial commit

---
+++ android/src/com/dath/learning/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dath.learning.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dath.learning.game.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
